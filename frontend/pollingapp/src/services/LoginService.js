85e1be60031e439241adf8ad74f6b84f717b1235
Jean Paul Sierra <jeanpaulsierraboom@gmail.com>
2023-02-27 08:40:38 -0500

    changed base endpoint

--- frontend/pollingapp/src/services/LoginService.js
+++ frontend/pollingapp/src/services/LoginService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const apiClient = axios.create({
-    baseURL:"http://127.0.0.1:5000/api",
+    baseURL:"http://concursoupb.com/api",
     headers:{
         Accept: 'application/json',
         "Content-Type": "multipart/form-data",

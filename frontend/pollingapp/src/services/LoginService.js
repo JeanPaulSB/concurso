c5283abd863e9927d1de3c580c3c672ce04bff99
Jean Paul Sierra <jeanpaulsierraboom@gmail.com>
2023-02-28 18:10:39 -0500

    fixing api endpoint

--- frontend/pollingapp/src/services/LoginService.js
+++ frontend/pollingapp/src/services/LoginService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const apiClient = axios.create({
-    baseURL:"http://192.168.1.7:5000/api",
+    baseURL:"https://www.concursoupb.com/api",
     headers:{
         Accept: 'application/json',
         "Content-Type": "multipart/form-data",
